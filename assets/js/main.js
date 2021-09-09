c95fafd80b5908fa8425e1e9b4c20823dab413d9
Anshu <anshumeena1947@gmail.com>
2021-09-09 12:03:39 +0530

    ready to uplaod

---
+++ assets/js/main.js
@@ -0,0 +1,53 @@
+var h1 = document.getElementById("heading");
+var text = [
+  "🤓 😰 🤕 🤠 😿",
+  "😡 🤯 🥵 🥶 👿",
+  "😇 😋 🤪 🤩 💩",
+  "🥳 🤬 😱 🤢 😈",
+  "🤩 🤮 👾 🤖 🎃",
+  "👹 👻 💀 ☠️ 👽",
+  "😂 😊 😤 😴 🤡",
+  "🥸 🤗 🤐 🤮 🤒",
+  "🥰 😝 😒 😟 😫",
+  "🍎 🍩 🍺 🥂 🍷",
+];
+
+let rendom = Math.floor(Math.random() * text.length);
+
+h1.innerText = text[rendom];
+
+function white() {
+  console.log("White Mode Activated");
+  let body = document.getElementsByTagName("body");
+  let text = document.getElementsByTagName("p");
+  let button = document.getElementById("style");
+
+  body[0].style.backgroundColor = "#fff";
+  text[0].style.color = "#111920";
+
+  let darkButton =
+    '<button style="background-color: transparent; border-color: transparent; cursor: pointer;" onclick="dark()"><img src="assets/Images/moon-outline.svg" height="32rem" width="32rem"/></button>';
+
+  button.innerHTML = darkButton;
+}
+
+function dark() {
+  console.log("Dark Mode Activated");
+  let body = document.getElementsByTagName("body");
+  let text = document.getElementsByTagName("p");
+  let button = document.getElementById("style");
+
+  body[0].style.backgroundColor = "#111920";
+  text[0].style.color = "#fff";
+
+  let darkButton =
+    '<button style="background-color: transparent; border-color: transparent; cursor: pointer;" onclick="white()"><img src="assets/Images/sun-outline.svg" height="32rem" width="32rem"/></button>';
+
+  button.innerHTML = darkButton;
+}
+
+function loadEmoji() {
+  console.log("Emojis Changed");
+  let rendom = Math.floor(Math.random() * text.length);
+  h1.innerText = text[rendom];
+}
