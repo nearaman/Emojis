513f277234aa8d2d08d2fcb3fa9f26f337442da1
Anshu <anshumeena1947@gmail.com>
2021-09-12 12:09:09 +0530

    add audios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,65 @@
+//for backgroung sound volume
+var audio = document.getElementById("audio2").volume=0.1;
+
+//chang emojis on relode automatically
 var h1 = document.getElementById("heading");
 var text = [
-  "🤓 😰 🤕 🤠 😿",
   "😡 🤯 🥵 🥶 👿",
-  "😇 😋 🤪 🤩 💩",
   "🥳 🤬 😱 🤢 😈",
-  "🤩 🤮 👾 🤖 🎃",
-  "👹 👻 💀 ☠️ 👽",
-  "😂 😊 😤 😴 🤡",
-  "🥸 🤗 🤐 🤮 🤒",
-  "🥰 😝 😒 😟 😫",
   "🍎 🍩 🍺 🥂 🍷",
+  "🌭 🍔 🍟 🍕 🍣",
+  "🍇 🧀 🍹 🪀 🏓",
+  "🍦 🍷 🥃 🍸 🧊",
+  "🎄 🥀 🌳 🌷 🪵",
+  "🌱 🌹 ☘️ 🌸 🎍",
+  "🌾 💐 🌺 🌼 🔥",
+  "💫 ✨ ❄️ 💧 💦",
+  "🍏 🍎 🍐 🍊 🍋",
+  "🎨 🥁 🎲 🚲 ✈️",
+  "📱 💾 💿 🔦 📕",
+  "👿 😱 🤯 🌹 🔥"
 ];
-
 let rendom = Math.floor(Math.random() * text.length);
-
 h1.innerText = text[rendom];
+
+// change emojis on click "p"
+
+function loadEmoji() {
+  let rendom = Math.floor(Math.random() * text.length);
+  h1.innerText = text[rendom];
+}
+
+// Modes
+
+// 1. Dark Mode
 
 function dark() {
   let body = document.getElementsByTagName("body");
   let text = document.getElementsByTagName("p");
   let button = document.getElementById("style");
 
+  var audio = document.getElementById("audio");
+  audio.volume = 0.2;
+  audio.play();
+
   body[0].style.backgroundColor = "#111920";
   text[0].style.color = "#fff";
 
   let darkButton =
-    '<button style="background-color: transparent; border-color: transparent; cursor: pointer;" onclick="white()"><img src="assets/images/sun-outline.svg" height="32rem" width="32rem"/></button>';
+    '<button style="background-color: transparent; border-color: transparent; cursor: pointer;" onclick="white()" ><img src="assets/images/sun-outline.svg" height="32rem" width="32rem"/></button>';
 
   button.innerHTML = darkButton;
 }
+
+// 2.White Mode
 
 function white() {
   let body = document.getElementsByTagName("body");
   let text = document.getElementsByTagName("p");
   let button = document.getElementById("style");
+  var audio = document.getElementById("audio");
+  audio.volume = 0.2;
+  audio.play();
 
   body[0].style.backgroundColor = "#fff";
   text[0].style.color = "#111920";
@@ -44,7 +70,9 @@ function white() {
   button.innerHTML = darkButton;
 }
 
-function loadEmoji() {
-  let rendom = Math.floor(Math.random() * text.length);
-  h1.innerText = text[rendom];
+// Click Sound
+function play() {
+  var audio = document.getElementById("audio");
+  audio.volume = 0.2;
+  audio.play();
 }
